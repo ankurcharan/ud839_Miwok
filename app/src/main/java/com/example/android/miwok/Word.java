166d8dbf1b0d930bf716d561e860c08c2e31eacb
Ankur Charan <ankurcharan98@gmail.com>
2020-06-21 06:37:24 +0530

    adding image check for phrases
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -2,13 +2,26 @@ package com.example.android.miwok;
 
 public class Word {
 
+    private static final int NO_IMAGE_PROVIDED =  -1;
+
     private String defaultTranslation;
     private String miwokTranslation;
+    private int imageResourceId = NO_IMAGE_PROVIDED;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
     }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public boolean hasImage() { return  imageResourceId != NO_IMAGE_PROVIDED; }
+
+    public int getImageResourceId() { return imageResourceId; }
 
     public String getDefaultTranslation() {
         return defaultTranslation;
